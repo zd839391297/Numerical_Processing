5fcb4d472f948d6e8f8e8563624f0bab1650844e
zd839391297 <zd839391297@outlook.com>
2018-11-06 16:17:29 +0800

    fit some bugs
    reshape the root.py

--- ROOT.py
+++ ROOT.py
@@ -1,96 +1,111 @@
 # This program aims to Find the root of the linear equation
 
-from numpy import poly1d
 import numpy as np
 import sympy as sp
-
-# import the equation
-p = poly1d([1, -1, 0.2149, -0.01438])
-
-# input the iterative method
-
-
-def method(x):
-    p = poly1d([1, -0.2149, 0.01438])
-    return p(x)**(1/3.0)
-
-
-# input the Error limit
-delta = 0.00005
-
-# search initial numbers
-a = 0.0
-b = 0.1
-step = 0.1
-while p(a)*p(b) > 0:
-    a += 0.2
-    a, b = b, a
-    print(p(b))
-print(a, b)
-
 # Dichotomy
-i = np.array([a, b])
-while abs(i[0]-i[1]) > delta:
-    temp = (i[0]+i[1])/2
-    if p(temp)*p(i[0]) > 0:
-        i[0] = temp
-    else:
-        i[1] = temp
-    print(i)
-print("the result of Dichotomy is", i[0])
+
+
+def Dichotomy(a, b, delta, p):
+    i = np.array([a, b])
+    while abs(i[0]-i[1]) > delta:
+        temp = (i[0]+i[1])/2
+        if p(temp)*p(i[0]) > 0:
+            i[0] = temp
+        else:
+            i[1] = temp
+        # print(i)
+    print("the result of Dichotomy is", i[0])
 
 # Iterative method
-i = np.array([a, b])
-while abs(i[1]-i[0]) > delta:
-    i[0] = method(i[1])
-    print(i)
-    i[0], i[1] = i[1], i[0]
-print("the result of Iterative method is", i[1])
+
+
+def Iterative(a, b, delta, p, method):
+    i = np.array([a, b])
+    while abs(i[1]-i[0]) > delta:
+        i[0] = method(i[1])
+        # print(i)
+        i[0], i[1] = i[1], i[0]
+    print("the result of Iterative is", i[1])
 
 # Wegstein
-i = np.array([a, b, 0])
 
 
-def method_we(x1, x2):
-    return (x2*method(x1)-x1*method(x2))/((x2-x1)-(method(x2)-method(x1)))
+def Wegstein(a, b, delta, p, method):
+    i = np.array([a, b, 0])
 
+    def method_we(x1, x2):
+        return (x2*method(x1)-x1*method(x2))/((x2-x1)-(method(x2)-method(x1)))
 
-while abs(i[0]-i[1]) > delta:
-    i[2] = method_we(i[0], i[1])
-    print(i)
-    i[0], i[1], i[2] = i[1], i[2], i[0]
-print("the result of Wegstein method is", i[1])
+    while abs(i[0]-i[1]) > delta:
+        i[2] = method_we(i[0], i[1])
+        # print(i)
+        i[0], i[1], i[2] = i[1], i[2], i[0]
+    print("the result of Wegstein method is", i[1])
 
 # Newton
-i = np.array([a, b])
 
 
-def method_no(x):
-    m = sp.symbols("m")
-    f = p(m)
-    return x-p(x)/(f.diff().evalf(subs={m: x}))
+def Newton(a, b, delta, p):
+    i = np.array([a, b])
 
+    def method_no(x):
+        m = sp.symbols("m")
+        f = p(m)
+        return x-p(x)/(f.diff().evalf(subs={m: x}))
 
-while abs(i[0]-i[1]) > delta:
-    i[0] = method_no(i[1])
-    print(i)
-    i[0], i[1] = i[1], i[0]
-print("the result of Newton method is", i[1])
+    while abs(i[0]-i[1]) > delta:
+        i[0] = method_no(i[1])
+        # print(i)
+        i[0], i[1] = i[1], i[0]
+    print("the result of Newton method is", i[1])
 
 # Chord cut
-i = np.array([a, b, 0])
 
 
-def method_ch(x1, x2):
-    return x2-p(x2)*(x2-x1)/(p(x2)-p(x1))
+def Chord(a, b, delta, p):
+    i = np.array([a, b, 0])
 
+    def method_ch(x1, x2):
+        return x2-p(x2)*(x2-x1)/(p(x2)-p(x1))
 
-while abs(i[0]-i[1]) > delta:
-    i[2] = method_ch(i[0], i[1])
-    print(i)
-    i[0], i[1], i[2] = i[1], i[2], i[0]
-print("the result of Chord cut method is", i[1])
+    while abs(i[0]-i[1]) > delta:
+        i[2] = method_ch(i[0], i[1])
+        # print(i)
+        i[0], i[1], i[2] = i[1], i[2], i[0]
+    print("the result of Chord cut method is", i[1])
 
 # sympy
-x = sp.symbols("x")
-print("the result of sympy is", sp.solve(p(x)))
+
+
+def sympy_m(p):
+    x = sp.symbols("x")
+    print("the result of sympy is", sp.solve(p(x)))
+
+
+if __name__ == "__main__":
+    # import the equation
+    def p(x):
+        return (x**3-x**2+0.2149*x-0.01438)
+
+    # input the iterative method
+
+    def method(x):
+        return (x**2-0.2149*x+0.01438)**(1/3.0)
+
+    # input the Error limit
+    delta = 0.00005
+
+    # search initial numbers
+    a = 0.0
+    b = 0.1
+    step = 0.1
+    while p(a)*p(b) > 0:
+        a += 0.2
+        a, b = b, a
+        # print(p(b))
+    Dichotomy(a, b, delta, p)
+    Iterative(a, b, delta, p, method)
+    Wegstein(a, b, delta, p, method)
+    Newton(a, b, delta, p)
+    Chord(a, b, delta, p)
+    sympy_m(p)
--- test.py
+++ test.py
@@ -1,9 +1,8 @@
 # This is a test document
-
-
 import NI
 import LS
 import LI
+import ROOT
 
 
 data_txt = "point_li.txt"
@@ -12,3 +11,26 @@ data_txt = "point_ls.txt"
 LS.ls(data_txt)
 data_txt = 'point_ni.txt'
 NI.ni(data_txt)
+from numpy import poly1d
+p = poly1d([1, -1, 0.2149, -0.01438])
+
+
+def method(x):
+    p = poly1d([1, -0.2149, 0.01438])
+    return p(x)**(1/3.0)
+
+
+ROOT.Dichotomy(0.7, 0.8, 0.00005, p)
+ROOT.Iterative(0.7, 0.8, 0.00005, p, method)
+ROOT.Wegstein(0.7, 0.8, 0.00005, p, method)
+ROOT.Newton(0.7, 0.8, 0.00005, p)
+ROOT.Chord(0.7, 0.8, 0.00005, p)
+ROOT.sympy_m(p)
+
+
+def q(x):
+    return x**2-1
+
+
+ROOT.Dichotomy(0.0, 2.0, 0.0005, q)
+ROOT.Newton(0.0, 2.0, 0.0005, q)
